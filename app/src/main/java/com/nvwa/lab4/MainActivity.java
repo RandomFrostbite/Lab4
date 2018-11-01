56cd751eb46586fc2e6417745bb2598bb4b772e2
RandomFrostbite <il.zainutdinov@gmail.com>
2018-11-02 00:58:59 +0100

    1-11 done

--- app/src/main/java/com/nvwa/lab4/MainActivity.java
+++ app/src/main/java/com/nvwa/lab4/MainActivity.java
@@ -1,7 +1,12 @@
 package com.nvwa.lab4;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -19,5 +24,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addClick(View view) {
+        String task_title = ( (EditText) findViewById(R.id.taskTitle ) ).getText().toString();
+        String task_desc  = ( (EditText) findViewById(R.id.taskDesc  ) ).getText().toString();
+
+        if ( task_desc.isEmpty() && task_title.isEmpty() )
+            myTasks.add( new Task( getResources().getString(R.string.defTitle), getResources().getString(R.string.defDesc) ) );
+        else {
+            if ( task_title.isEmpty() )
+                task_title = getResources().getString(R.string.defTitle);
+            if ( task_desc.isEmpty() )
+                task_desc = getResources().getString(R.string.misDesc);
+            myTasks.add( new Task( task_title, task_desc ) );
+        }
+
+        TaskListFragment taskFr = (TaskListFragment) getSupportFragmentManager().findFragmentById(R.id.taskFragment);
+
+        ArrayAdapter<Task> taskAdapter = (ArrayAdapter<Task>) taskFr.getListAdapter();
+
+        taskAdapter.notifyDataSetChanged();
+
+        ((EditText) findViewById(R.id.taskTitle)).setText(null);
+        ((EditText) findViewById(R.id.taskDesc)).setText(null);
+
+        View focusedView = getCurrentFocus();
+        if ( focusedView != null ) {
+            InputMethodManager im = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
---
+++ app/src/main/java/com/nvwa/lab4/TaskListFragment.java
@@ -0,0 +1,30 @@
+package com.nvwa.lab4;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TaskListFragment extends ListFragment {
+
+
+    public TaskListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setListAdapter( new ArrayAdapter<Task>( getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, MainActivity.myTasks ) );
+    }
+
+}
